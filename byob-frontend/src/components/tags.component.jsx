b3249de299432a2c5fa3fdb03caa3da565f0d45c
matheesha-20 <matheeshakavindu32@gmail.com>
2025-09-26 20:28:31 +0200

    feat: enhance Tags component to support tag editing and indexing

--- byob-frontend/src/components/tags.component.jsx
+++ byob-frontend/src/components/tags.component.jsx
@@ -4,7 +4,7 @@ import { EditorContext } from "../pages/editor.pages";
 
 
 
-const Tags = ({ tag }) => {
+const Tags = ({ tag, tagIndex }) => {
 
 let { blog, blog: { tags }, setBlog } = useContext(EditorContext);
 
@@ -12,14 +12,36 @@ let { blog, blog: { tags }, setBlog } = useContext(EditorContext);
 const handleTagDelete = () => {
 
     tags = tags.filter(t => t !== tag);
-    setBlog({ ...blog, tags: tags})
+    setBlog({ ...blog, tags: tags});
 
+}
+
+const handleTagEdit = (e) => {
+    if (e.keyCode == 13 || e.keyCode == 188) {
+        
+        e.preventDefault();
+
+        let currentTag = e.target.innerText;
+
+        tags[tagIndex] = currentTag;
+
+        setBlog({ ...blog, tags });
+
+        e.target.setAttribute("contentEditable", false)
+
+    }
+
+}
+
+const addEditable = (e) => {
+    e.target.setAttribute("contentEditable", true);
+    e.target.focus();
 }
 
 
     return (
         <div className="relative p-1  mt-1 mr-1 pl-4 mb-1 px-1 rounded-full inline-block bg-slate-800  hover:bg-opacity-90 pr-10">
-            <p className="outline-none" contentEditable="true">{tag}</p>
+            <p className="outline-none" onKeyDown={handleTagEdit} onClick={addEditable}>{tag}</p>
             <button className="mt-[2px] rounded-full absolute right-3 top-1/2 -translate-y-1/2"
                     onClick={handleTagDelete}
             >
